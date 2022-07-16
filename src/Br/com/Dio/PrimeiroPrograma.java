8e1ac26635072f7b8ef581a4ba9d90e7e4ec80f2
perkles <jociele@hotmail.com>
2022-07-16 09:31:24 -0300

    first commit

---
+++ src/Br/com/Dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package Br.com.Dio;
+
+import Br.com.Dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new gato();
+        System.out.println(gato);
+
+       /*int a = 5;
+        int b = 5;
+        System.out.println("Hello World " + (a+b));*/
+    }
+}
