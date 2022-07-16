67016382be594a974fd778421f164fb15da7cb58
perkles <jociele@hotmail.com>
2022-07-16 09:44:23 -0300

    Criando Class Livro

--- src/Br/com/Dio/PrimeiroPrograma.java
+++ src/Br/com/Dio/PrimeiroPrograma.java
@@ -7,8 +7,38 @@ public class PrimeiroPrograma {
         Gato gato = new gato();
         System.out.println(gato);
 
+        Livro livro = new livro (nome "o Problema dos 3 corpos", Npag "300");
+        System.out.println(Livro);
+
        /*int a = 5;
         int b = 5;
         System.out.println("Hello World " + (a+b));*/
+    }
+}
+class Livro {
+    private String nome;
+    private Integer Npag;
+
+    public Livro() {    }
+
+    public Livro(String nome, Integer npag) {
+        this.nome = nome;
+        Npag = npag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpag() {
+        return Npag;
+    }
+
+    public void setNpag(Integer npag) {
+        Npag = npag;
     }
 }
